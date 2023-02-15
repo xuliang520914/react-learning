a2c354e8a08e06270beb7f436068d591380ec801
xuliang <xuliang@lollicupchina.com>
2023-02-15 09:40:05 +0800

    看板:    1:默认不显示“添加新卡片”卡片;    2:点击顶部“添加新卡片”按钮，“添加新卡片”卡片才会出现;    3:在出现的文本框中输入卡片标题，按回车即在待处理看板列顶部新插入一张卡片，这张卡片的标题为刚才文本框的输入    4:在新卡片插入同时，“添加新卡片”卡片消失，回到 1 的状态

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+
+const todoList = [ 
+  { title: '开发任务-1', status: '22-05-22 18:15' }, 
+  { title: '开发任务-3', status: '22-05-22 18:15' }, 
+  { title: '开发任务-5', status: '22-05-22 18:15' }, 
+  { title: '测试任务-3', status: '22-05-22 18:15' }
+];
+const ongoingList = [ 
+  { title: '开发任务-4', status: '22-05-22 18:15' }, 
+  { title: '开发任务-6', status: '22-05-22 18:15' }, 
+  { title: '测试任务-2', status: '22-05-22 18:15' }
+];
+const doneList = [ 
+  { title: '开发任务-2', status: '22-05-22 18:15' }, 
+  { title: '测试任务-1', status: '22-05-22 18:15' }
+];
+
+const KanbanCard = ({title, status}) => {
+  return (
+    <li className="kanban-card">
+      <div className="card-title">{title}</div>
+      <div className="card-status">{status}</div>
+    </li>
+  )
+}
+
+const KanbanNewCard = ({onSubmit}) => {
+  const [title, setTitle] = useState('');
+  
+  const handleChange = (evt) => {
+    setTitle(evt.target.value);
+  }
+
+  const handleKeyDown = (evt) => {
+    if (evt.key === 'Enter') {
+      onSubmit(title);
+    }
+  };
+  return (
+    <li className="kanban-card">
+      <h3>添加新卡片</h3>
+      <div className="card-title">
+        <input type="text" value={title} 
+          onChange={handleChange} onKeyDown={handleKeyDown}/>
+      </div>
+    </li>
+  )
+}
 
 function App() {
+  const [showAdd, setShowAdd] = useState(false)
+  
+  
+
+  const handleAdd = (env) => {
+    setShowAdd(true)
+  };
+
+  const handleSubmit = (title) => {
+    todoList.unshift({title, status: new Date().toDateString()});
+    setShowAdd(false)
+    
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1>我的看板</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
+      <main className="kanban-board">
+
+      <section className="kanban-column column-todo">
+        <h2>待处理<button onClick={handleAdd} 
+        disabled={showAdd}>&#8853; 添加新卡片</button></h2>
+        <ul>
+          {/* <KanbanNewCard /> */}
+          {showAdd && <KanbanNewCard onSubmit={handleSubmit} /> }
+          {todoList.map((props, index) => <KanbanCard {...props} key={index}/>)}
+        </ul>
+      </section>
+
+        
+        <section className="kanban-column column-ongoing">
+          <h2>进行中</h2>
+          <ul>
+            {
+              ongoingList.map((props, index) => <KanbanCard {...props} key={index}/>)
+            }
+          </ul>
+        </section>
+        <section className="kanban-column column-done">
+          <h2>已完成</h2>
+          <ul>
+            {
+              doneList.map((props, index) => <KanbanCard {...props} key={index}/>)
+            }
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
